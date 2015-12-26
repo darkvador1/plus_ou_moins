54c96a507439e8a2e1e2f8e582d5598bc65fff8a
Landryl <landryl@users.noreply.github.com>
2015-12-26 16:33:11 +0200

    Version 2.1

--- plus_ou_moins.py
+++ plus_ou_moins.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3.4
 # -*-coding:Utf-8 -*
-# Jeu "PLUS OU MOINS" - Version 1.2
+# Jeu "PLUS OU MOINS" - Version 2.1
 from random import randint
 
 nombreADeviner = randint(30,100)
 print("_PHASE_TEST_ nombreADeviner :",nombreADeviner) #Affichage uniquement en phase de test/debug
 nombreJoueur = 0
+nombreTentatives = 0
 while nombreJoueur!=nombreADeviner : #Le jeu tourne tant que le joueur a choisi un nombre different du juste prix
 
 	nombreJoueur = input("Essayez de deviner le nombre entre 30 et 100 : ")
@@ -16,9 +17,12 @@ while nombreJoueur!=nombreADeviner : #Le jeu tourne tant que le joueur a choisi 
 			print("Il faut choisir un nombre entre 30 et 100.")
 		elif nombreJoueur < nombreADeviner :
 			print("Ce n'est pas le bon nombre, essayez plus grand.")
+			nombreTentatives = nombreTentatives + 1
 		elif nombreJoueur > nombreADeviner :
 			print("Ce n'est pas le bon nombre, essayez plus petit.")
+			nombreTentatives = nombreTentatives + 1
 		else :
 			print("Bravo, vous avez trouvé le juste prix. On vous offre un sejour à Paris.")
 	else :
 		print("Entrez un nombre entier !")
+print("Vous avez eu besoin de",nombreTentatives,"tentative(s).")
