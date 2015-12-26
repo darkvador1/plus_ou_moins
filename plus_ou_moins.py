5b169acf3d4f7950522c0a6b6ec6503e023bbac4
Landryl <landryl@users.noreply.github.com>
2015-12-26 16:26:55 +0200

    Version 1.2 - fixes #1

--- plus_ou_moins.py
+++ plus_ou_moins.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3.4
 # -*-coding:Utf-8 -*
-# Jeu "PLUS OU MOINS" - Version 1.1
+# Jeu "PLUS OU MOINS" - Version 1.2
 from random import randint
 
 nombreADeviner = randint(30,100)
@@ -8,14 +8,17 @@ print("_PHASE_TEST_ nombreADeviner :",nombreADeviner) #Affichage uniquement en p
 nombreJoueur = 0
 while nombreJoueur!=nombreADeviner : #Le jeu tourne tant que le joueur a choisi un nombre different du juste prix
 
-	nombreJoueur = int(input("Essayez de deviner le nombre entre 30 et 100 : "))
+	nombreJoueur = input("Essayez de deviner le nombre entre 30 et 100 : ")
 
-	if nombreJoueur<30 or nombreJoueur>100 :
-		print("Il faut choisir un nombre entre 30 et 100.")
-	elif nombreJoueur < nombreADeviner :
-		print("Ce n'est pas le bon nombre, essayez plus grand.")
-	elif nombreJoueur > nombreADeviner :
-		print("Ce n'est pas le bon nombre, essayez plus petit.")
+	if nombreJoueur.isdigit() :              # On verfie que nombreJoueur est bien un nombre
+		nombreJoueur = int(nombreJoueur) # Pour ensuite le convertir en un entier
+		if nombreJoueur<30 or nombreJoueur>100 :
+			print("Il faut choisir un nombre entre 30 et 100.")
+		elif nombreJoueur < nombreADeviner :
+			print("Ce n'est pas le bon nombre, essayez plus grand.")
+		elif nombreJoueur > nombreADeviner :
+			print("Ce n'est pas le bon nombre, essayez plus petit.")
+		else :
+			print("Bravo, vous avez trouvé le juste prix. On vous offre un sejour à Paris.")
 	else :
-		print("Bravo, vous avez trouvé le juste prix. On vous offre un sejour à Paris.")
-
+		print("Entrez un nombre entier !")
