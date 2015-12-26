1e1b85f7387d46895a848776af747bad3666cbfc
Landryl <landryl@users.noreply.github.com>
2015-12-26 16:40:12 +0200

    Changement du nom de la variable nombreJoueur vers nombreDuJoueur

--- plus_ou_moins.py
+++ plus_ou_moins.py
@@ -5,20 +5,20 @@ from random import randint
 
 nombreADeviner = randint(30,100)
 print("_PHASE_TEST_ nombreADeviner :",nombreADeviner) #Affichage uniquement en phase de test/debug
-nombreJoueur = 0
+nombreDuJoueur = 0
 nombreTentatives = 0
-while nombreJoueur!=nombreADeviner : #Le jeu tourne tant que le joueur a choisi un nombre different du juste prix
+while nombreDuJoueur!=nombreADeviner : #Le jeu tourne tant que le joueur a choisi un nombre different du juste prix
 
-	nombreJoueur = input("Essayez de deviner le nombre entre 30 et 100 : ")
+	nombreDuJoueur = input("Essayez de deviner le nombre entre 30 et 100 : ")
 
-	if nombreJoueur.isdigit() :              # On verfie que nombreJoueur est bien un nombre
-		nombreJoueur = int(nombreJoueur) # Pour ensuite le convertir en un entier
-		if nombreJoueur<30 or nombreJoueur>100 :
+	if nombreDuJoueur.isdigit() :              # On verfie que nombreDuJoueur est bien un nombre
+		nombreDuJoueur = int(nombreDuJoueur) # Pour ensuite le convertir en un entier
+		if nombreDuJoueur<30 or nombreDuJoueur>100 :
 			print("Il faut choisir un nombre entre 30 et 100.")
-		elif nombreJoueur < nombreADeviner :
+		elif nombreDuJoueur < nombreADeviner :
 			print("Ce n'est pas le bon nombre, essayez plus grand.")
 			nombreTentatives = nombreTentatives + 1
-		elif nombreJoueur > nombreADeviner :
+		elif nombreDuJoueur > nombreADeviner :
 			print("Ce n'est pas le bon nombre, essayez plus petit.")
 			nombreTentatives = nombreTentatives + 1
 		else :
